989b18ff603f222ed309b622e5170ff8fd0a962b
leonram1245 <marnoeldeguzman@gmail.com>
2025-06-08 22:26:38 +0800

    create server entrypoint

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import taskRoutes from "./routes/task.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/api/tasks", taskRoutes);
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
