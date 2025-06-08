015aaceb79f52161de5ad007a4c6e1bd13f94021
leonram1245 <marnoeldeguzman@gmail.com>
2025-06-08 22:32:36 +0800

    add slices

---
+++ frontend/src/features/ui/uiSlice.js
@@ -0,0 +1,20 @@
+// features/ui/uiSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  activeTab: "login",
+};
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState,
+  reducers: {
+    setActiveTab: (state, action) => {
+      state.activeTab = action.payload;
+    },
+  },
+});
+
+export const { setActiveTab } = uiSlice.actions;
+
+export default uiSlice.reducer;
