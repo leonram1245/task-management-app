b784b88cd9b941164c7598d365a526bec4e61af6
leonram1245 <marnoeldeguzman@gmail.com>
2025-06-08 22:31:13 +0800

    generate create react app

---
+++ frontend/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/DashboardPage";
+const system = createSystem(defaultConfig, { theme: { tokens: {} } });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ChakraProvider value={system}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
+    </Provider>
+  </React.StrictMode>
+);
