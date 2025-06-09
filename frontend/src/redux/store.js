24d31b6cd781c144d689b362f0547f74b73dd30f
leonram1245 <marnoeldeguzman@gmail.com>
2025-06-10 02:28:27 +0800

    add userSearchReducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,13 +1,15 @@
 // store.js
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth/authSlice";
-import uiReducer from "../features/ui/uiSlice";
-import taskReducer from "../features/ui/taskSlice";
+import authReducer from "../features/authSlice";
+import uiReducer from "../features/uiSlice";
+import taskReducer from "../features/taskSlice";
+import userSearchReducer from "../features/userSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     ui: uiReducer,
     tasks: taskReducer,
+    userSearch: userSearchReducer,
   },
 });
