ba5a341fc30f265f80ac290169fe93c255aafbc3
leonram1245 <marnoeldeguzman@gmail.com>
2025-06-10 02:26:12 +0800

    modify payload for task service

--- backend/service/task.service.js
+++ backend/service/task.service.js
@@ -1,22 +1,52 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export const createTaskService = async ({ title, description, userId }) => {
+export const createTaskService = async ({
+  title,
+  description,
+  assignedBy,
+  assignedTo,
+  status,
+}) => {
   return await prisma.task.create({
-    data: { title, description, userId },
+    data: {
+      title,
+      description,
+      status: status || "TODO",
+      assignedBy: { connect: { id: assignedBy } },
+      assignedTo: { connect: { id: assignedTo } },
+    },
   });
 };
 
 export const getTasksService = async (userId) => {
-  return await prisma.task.findMany({
-    where: { userId },
+  const createdTasks = await prisma.task.findMany({
+    where: { assignedById: userId },
   });
+  const assignedTasks = await prisma.task.findMany({
+    where: { assignedToId: userId },
+  });
+
+  return { createdTasks, assignedTasks };
 };
 
-export const updateTaskService = async ({ id, title, description, userId }) => {
-  return await prisma.task.updateMany({
-    where: { id: parseInt(id), userId },
-    data: { title, description },
+export const updateTaskService = async ({
+  id,
+  title,
+  description,
+  status,
+  userId,
+}) => {
+  const task = await prisma.task.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  if (!task) throw new Error("Task not found");
+  if (task.assignedById !== userId) throw new Error("Not authorized");
+
+  return await prisma.task.update({
+    where: { id: parseInt(id) },
+    data: { title, description, status },
   });
 };
 
