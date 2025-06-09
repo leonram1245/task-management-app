1291c7b0c57475146577cd212fa98824e85cdbd9
leonram1245 <marnoeldeguzman@gmail.com>
2025-06-10 02:25:06 +0800

    add task status field

--- backend/prisma/migrations/20250608180933_init/migration.sql
+++ backend/prisma/migrations/20250608180933_init/migration.sql
@@ -3,6 +3,8 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
