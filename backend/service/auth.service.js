6f3ca851e56f4221fc655dacd3920a184043b039
leonram1245 <marnoeldeguzman@gmail.com>
2025-06-10 02:25:30 +0800

    modify payload for auth service

--- backend/service/auth.service.js
+++ backend/service/auth.service.js
@@ -2,10 +2,15 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
-export const signupService = async ({ email, password }) => {
-  console.log(email, password);
+export const signupService = async ({
+  email,
+  password,
+  firstName,
+  lastName,
+}) => {
   const existingUser = await prisma.user.findUnique({ where: { email } });
   if (existingUser) {
     throw new Error("Email already exists");
@@ -15,15 +20,34 @@ export const signupService = async ({ email, password }) => {
 
   const newUser = await prisma.user.create({
     data: {
+      firstName,
+      lastName,
       email,
       password: hashedPassword,
     },
   });
 
-  return { message: "User registered successfully", userId: newUser.id };
+  const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  return {
+    token,
+    user: {
+      id: newUser.id,
+      email: newUser.email,
+      firstName: newUser.firstName,
+      lastName: newUser.lastName,
+    },
+  };
 };
 
-export const loginService = async ({ email, password }) => {
+export const loginService = async ({
+  email,
+  password,
+  firstName,
+  lastName,
+}) => {
   const user = await prisma.user.findUnique({ where: { email } });
   if (!user || !(await bcrypt.compare(password, user.password))) {
     throw new Error("Invalid credentials");
@@ -33,5 +57,13 @@ export const loginService = async ({ email, password }) => {
     expiresIn: "1h",
   });
 
-  return { token };
+  return {
+    token,
+    user: {
+      id: user.id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+  };
 };
